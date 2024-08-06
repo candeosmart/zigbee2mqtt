29ac9443731c4779a7985551df4229dba8cdb224
dhc25 <66464694+dhc25@users.noreply.github.com>
2024-08-06 21:44:18 +0100

    Update candeo_c202v2.js
    Removed With or Without N

--- candeo_c202v2.js
+++ candeo_c202v2.js
@@ -5,7 +5,7 @@ const definition = {
                     { modelID: 'C202', softwareBuildID: '1.04', dateCode: '20230828' } ],
     model: 'C202',
     vendor: 'Candeo',
-    description: 'Candeo C202 / C202N Zigbee rotary dimmer (with or without neutral)',
+    description: 'Candeo C202 / C202N Zigbee rotary dimmer',
     extend: [ light( { 'configureReporting': true, levelConfig: { disabledFeatures: [ 'on_transition_time', 'off_transition_time', 'on_off_transition_time', 'execute_if_off' ] }, 'powerOnBehavior': true } ) ],
     meta: {},
 };
